a7e1388fbdcacda3fe158590cdefd8d960262a5e
HightMike <baobab-borisov@mail.ru>
2019-05-07 04:43:42 +0300

    3. Поиск наибольшего общего делителя через алгорим Евклида. (делением)

--- src/stepik/NOD/Nod.java
+++ src/stepik/NOD/Nod.java
@@ -6,25 +6,20 @@ public class Nod {
 
         System.out.println(a +" "+ b);
 
-        int max =  Math.max(a, b);
-        int min =  Math.min(a, b);
-
-
-
-
-        if (a!=b && a!=0 && b!=0) {
-            return calculate(min, max-min);
-        }
-        else if (b==0) {
+        if (a==0) {
             return b;
         }
-        else if (a==0) {
-            return a;
-        }
-        else {
+
+        if (b==0) {
             return a;
         }
 
+        if (a>=b) {
+            return calculate(a%b, b);
+        }
+        else {
+            return calculate(a, b%a);
+        }
 
     }
 
@@ -33,7 +28,7 @@ public class Nod {
         long start = System.currentTimeMillis();
 
         Nod nod = new Nod();
-        System.out.println(nod.calculate(12234234,234234234));
+        System.out.println(nod.calculate(12234234,23434));
 
         long finish = System.currentTimeMillis();
 
